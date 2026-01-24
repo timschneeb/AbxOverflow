5b6468becbd50d68ce7190c23d6df62e5f5dfcde
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-01-24 17:51:51 +0100

    Revert "Migrate to new gradle daemon"
    This reverts commit f0851fc2f6658da923b28df8ef55381de7a7764d.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,6 @@ pluginManagement {
         maven { url = uri("https://jitpack.io") }
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 
 dependencyResolutionManagement {
     versionCatalogs {
