f0851fc2f6658da923b28df8ef55381de7a7764d
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-01-24 17:45:27 +0100

    Migrate to new gradle daemon

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,9 @@ pluginManagement {
         maven { url = uri("https://jitpack.io") }
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     versionCatalogs {
